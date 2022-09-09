be3c7950e39e04d4a1e2a8c62bfec852b106efc8
uswa.r <uswa.r@outlook.com>
2022-09-10 03:57:27 +0500

    .

--- script.js
+++ script.js
@@ -1,39 +1,72 @@
-//how to set date automatically
-const date = document.getElementById('date');
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
 
+// ********** set date ************
+// select span
+const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
-//date setupend here
 
-//close links - nav toggle button
-const navToggle = document.querySelector('.nav-toggle');
-const linksContainer = document.querySelector('.links-container');
-const links = document.querySelector('.links');
+// ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
-navToggle.addEventListener('click', function() {
-    // linksContainer.classList.toggle('show-links'); that's a manual way to show links which do not auto resize toggle container
-    //below is auto height resize method:
-    const containerHeight = linksContainer.getBoundingClientRect().height;
+navToggle.addEventListener("click", function() {
+    // linksContainer.classList.toggle("show-links");
+
     const linksHeight = links.getBoundingClientRect().height;
-    //dynamically adding height
+    const containerHeight = linksContainer.getBoundingClientRect().height;
     if (containerHeight === 0) {
         linksContainer.style.height = `${linksHeight}px`;
     } else {
-        linksHeight.style.height = 0;
+        linksContainer.style.height = 0;
+    }
+    // console.log(linksContainer.getBoundingClientRect());
+});
+
+// ********** fixed navbar ************
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function() {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+    // setup back to top link
+
+    if (scrollHeight > 500) {
+        console.log("helo");
+
+        topLink.classList.add("show-link");
+    } else {
+        topLink.classList.remove("show-link");
     }
 });
-//dynamically close links with  height ends here
+//select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
 
-//fixed navbar
-const navbar = document.getElementById(nav);
-const topLink = document.querySelector('.top-link');
+scrollLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        //prevent default
+        e.preventDefault();
+        //navigate to specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        let position = element.offsetTop;
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
 
-window.addEventListener('scroll', function() {
-    const scrollHeight = window.pageXOffset;
-    const navHeight = navbar.getBoundingClientRect().height;
-
-    if (scrollHeight > navHeight) {
-        navbar.classList.add('fixed-nav');
-    } else {
-        navbar.classList.remove('fixed-nav');
-    }
 });
