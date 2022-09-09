513d292cabe93c6f66c1d310386171c292d238a5
uswa.r <uswa.r@outlook.com>
2022-09-10 04:12:42 +0500

    .

--- script.js
+++ script.js
@@ -49,32 +49,3 @@ window.addEventListener("scroll", function() {
         topLink.classList.remove("show-link");
     }
 });
-//select links
-const scrollLinks = document.querySelectorAll('.scroll-link');
-
-scrollLinks.forEach(function(link) {
-    link.addEventListener('click', function(e) {
-        //prevent default
-        e.preventDefault();
-        //navigate to specific spot
-        const id = e.currentTarget.getAttribute('href').slice(1);
-        const element = document.getElementById(id);
-        const navHeight = navbar.getBoundingClientRect().height;
-        const containerHeight = linksContainer.getBoundingClientRect().height;
-        const fixedNav = navbar.classList.contains('fixed-class');
-        let position = element.offsetTop - navHeight;
-
-        if (!fixedNav) {
-            position = position - navHeight;
-        }
-        if (!fixedNav) {
-            position = position + containerHeight;
-        }
-        window.scrollTo({
-            left: 0,
-            top: position,
-        });
-        linksContainer.style.height = 0;
-    });
-
-});
